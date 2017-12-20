5bdb5a0cb2dc14d8b55d7434906f7a5cb5046c25
Andrea <andrea.wayte@gmail.com>
2017-12-20 12:59:49 -0800

    +=+= calculator function does not go past 8 for 1+1+=

--- calculator.js
+++ calculator.js
@@ -1,105 +1,3 @@
-var mathOperators =  {
-    '+': function(x,y){ return x + y},
-    '-': function(x,y){ return x - y},
-    '/': function(x,y){ return x / y},
-    '*': function(x,y){ return x * y}
-}
-
-function Value(type, value) {
-  this.type = type,
-  this.value = value
-}
-
-// var Calculation = {
-//
-// }
-
-var calculator = {
-  currentNumber: null,
-  currentOperator: null,
-  arrayOfValues: [],
-  displayScreen: [],
-  clearCalculator: function(){
-    this.arrayOfValues = [];
-    this.currentNumber = null;
-    this.currentOperator = null;
-    $('.input').text("");
-    calculator.displayScreen = [];
-  },
-  calculate: function(){
-    var newValue = new Value('number', this.currentNumber);
-    this.arrayOfValues.push(newValue);
-    this.currentNumber = null;
-    for(var i=0 ; i< this.arrayOfValues.length ; i++){
-      // var answer = getAnswer()
-      // debugger;
-      if(this.arrayOfValues[i].type === 'operand' && this.arrayOfValues[i+1]){
-        //get values before and after
-        var operator = this.arrayOfValues[i].value;
-        var num1 = parseFloat(this.arrayOfValues[i-1].value);
-        var num2 = parseFloat(this.arrayOfValues[i+1].value);
-        var equation = mathOperators[operator](num1, num2);
-        var answer = new Value('number', equation);
-        this.arrayOfValues.splice(i-1, 3, answer);
-        i = i-1;
-        // this.arrayOfValues[i-1] = answer;
-      }
-
-      if(equation === "Infinity"){
-        $('.input').text("error");
-        this.currentNumber = null;
-        calculator.displayScreen = [];
-      } else {
-        this.currentNumber = equation;
-        $('.input').text(equation);
-        calculator.displayScreen = [equation];
-      }
-      this.currentOperator = null;
-    }
-  },
-  pressNumberButton: function(){
-    var item = $(event.target).text();
-    if(this.currentOperator){
-      var operator = new Value('operand', this.currentOperator);
-      this.arrayOfValues.push(operator);
-      this.currentOperator = null;
-    }
-    if(this.currentNumber){
-      this.currentNumber += item;
-    } else {
-      this.currentNumber = item;
-    }
-    displayOnCalculator(item);
-  },
-  pressOperandButton: function(){
-    if(this.currentNumber){
-      var newValue = new Value('number', this.currentNumber);
-      this.arrayOfValues.push(newValue);
-      this.currentNumber = null;
-    }
-    var item = $(event.target).val();
-    if(!this.currentOperator){
-      displayOnCalculator(item);
-    } else if (this.currentOperator != item){
-      this.displayScreen.pop();
-      displayOnCalculator(item);
-    }
-    this.currentOperator = item;
-  },
-  addDecimalToNumber: function (){
-    if(this.currentNumber && this.currentNumber[this.currentNumber.length -1] !== '.'){
-      this.currentNumber += ".";
-      displayOnCalculator(".");
-    }
-  }
-}
-
-function getAnswer(){
-
-}
-
-  //idea: keep an array of operators used, sort them in order of operations and find those values
-
 
 $(document).ready(initializeApp);
 
@@ -114,4 +12,123 @@ function initializeApp(){
 function displayOnCalculator(item){
   calculator.displayScreen.push(item)
   $('.input').text(calculator.displayScreen.join(""));
+}
+
+var mathOperators =  {
+    '+': function(x,y){ return x + y},
+    '-': function(x,y){ return x - y},
+    '/': function(x,y){ return x / y},
+    '*': function(x,y){ return x * y}
+}
+
+function Value(type, value) {
+  this.type = type,
+  this.value = value
+}
+
+var calculator = {
+  lastFunction: {},
+  currentNumber: null,
+  currentOperator: null,
+  arrayOfInputValues: [],
+  displayScreen: [],
+  clearCalculator: function(){
+    this.arrayOfInputValues = [];
+    this.currentNumber = null;
+    this.currentOperator = null;
+    $('.input').text("");
+    calculator.displayScreen = [];
+  },
+  calculate: function(){
+    // debugger;
+    if(this.arrayOfInputValues.length === 1 && this.lastFunction.operand){
+        //take the first item in arrayOfInputValues
+        var currentSum = parseFloat(this.arrayOfInputValues[0].value)
+        var answer = mathOperators[this.lastFunction.operand]( currentSum, this.lastFunction.num2);
+        var newInput = new Value('number', answer);
+        this.arrayOfInputValues.pop();
+        this.arrayOfInputValues.push(newInput);
+        this.displayScreen.pop();
+        displayOnCalculator(answer);
+    } else if(this.arrayOfInputValues[this.arrayOfInputValues.length -1].type === 'operand'){
+      // debugger;
+      var operator = this.arrayOfInputValues.pop();
+      if(this.arrayOfInputValues.length !== 1){
+        getSumOfInput();
+      }
+      var result = mathOperators[operator.value](this.arrayOfInputValues[0].value, this.arrayOfInputValues[0].value);
+      var createResultObject = new Value('number', result);
+      this.arrayOfInputValues = [createResultObject];
+      this.displayScreen = [];
+      this.lastFunction.operand = operator.value;
+      this.lastFunction.num2 = result;
+      displayOnCalculator(result);
+    } else {
+      getSumOfInput();
+    }
+  },
+  pressNumberButton: function(){
+    var item = $(event.target).text();
+    if(this.currentNumber){
+      if(this.currentNumber[this.currentNumber.length -1] !== "."){
+        this.displayScreen.pop();
+      }
+      this.currentNumber += item;
+      this.arrayOfInputValues.pop();
+    } else {
+      this.currentNumber = item;
+    }
+    var newValue = new Value('number', this.currentNumber);
+    this.arrayOfInputValues.push(newValue);
+    displayOnCalculator(item);
+    this.currentOperator = null;
+  },
+  pressOperandButton: function(){
+    // debugger;
+    this.currentNumber = null;
+    var item = $(event.target).val();
+    if(this.currentOperator){
+      this.arrayOfInputValues.pop();
+      this.displayScreen.pop();
+    }
+    this.currentOperator = item;
+    var newValue = new Value('operand', this.currentOperator);
+    this.arrayOfInputValues.push(newValue);
+    displayOnCalculator(item);
+  },
+  addDecimalToNumber: function (){
+    if(this.currentNumber && this.currentNumber[this.currentNumber.length -1] !== '.'){
+      this.currentNumber += ".";
+      displayOnCalculator(".");
+    }
+  }
+}
+  //idea: keep an array of operators used, sort them in order of operations and find those values
+function getSumOfInput(){
+  for(var i=0 ; i< calculator.arrayOfInputValues.length ; i++){
+    if(calculator.arrayOfInputValues[i].type === 'operand' ){
+      //get values before and after
+      var operator = calculator.arrayOfInputValues[i].value;
+      var num1 = parseFloat(calculator.arrayOfInputValues[i-1].value);
+      var num2 = parseFloat(calculator.arrayOfInputValues[i+1].value);
+      var equation = mathOperators[operator](num1, num2);
+      var answer = new Value('number', equation);
+      calculator.arrayOfInputValues.splice(i-1, 3, answer);
+      i = i-1;
+    }
+
+    if(equation === "Infinity"){
+      $('.input').text("error");
+      calculator.currentNumber = null;
+      calculator.displayScreen = [];
+    } else {
+      calculator.currentNumber = equation;
+      $('.input').text(equation);
+      calculator.displayScreen = [equation];
+    }
+    calculator.lastFunction.operand = operator;
+    calculator.lastFunction.num2 = num2;
+    calculator.currentOperator = null;
+  }
+  return equation;
 }
